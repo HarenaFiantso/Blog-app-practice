9b44a292ece3d593ad64bae8c18496cba9303628
HarenaFiantso <fiantsorav@gmail.com>
2024-03-10 13:55:32 +0300

    style: home page styling

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,8 +4,9 @@ import {blogs} from "@/app/datas/BlogList";
 const HomePage = () => {
     return (
         <div className="flex items-center justify-center flex-col">
-            <h1 className="mb-8 font-bold text-2xl">Home</h1>
-            <ul>
+            <h1 className="mb-3 font-bold text-2xl">Home</h1>
+            <p className="mb-8">This is the home page</p>
+            <ul className="flex flex-col gap-5">
                 {blogs.map((blog) => (
                     <li key={blog.id}>
                         <Link href={`/blog/${blog.id}`} className="hover:underline">{blog.title}</Link>
