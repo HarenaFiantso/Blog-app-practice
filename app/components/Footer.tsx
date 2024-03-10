f4574940aef39c65ac3de599e3a6a69a0d455461
HarenaFiantso <fiantsorav@gmail.com>
2024-03-10 11:04:34 +0300

    feat: add Header and Footer components

---
+++ app/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <div className="w-full bg-gray-900 bottom-0 text-center py-5">
+            Footer
+        </div>
+    );
+}
---
+++ app/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <div className="w-full bg-gray-900 sticky top-0 right-0 left-0 text-center py-5">
+            Header
+        </div>
+    );
+}
