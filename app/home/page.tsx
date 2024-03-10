2a301e3f9ed57d0f0978aff8e3e3db793a103acf
HarenaFiantso <fiantsorav@gmail.com>
2024-03-10 11:33:42 +0300

    chore: add blog list to home

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 export default function HomePage() {
+    const blogs: string[] = ['Blog-1', 'Blog-2', 'Blog-3']
+
     return (
-        <div className="h-screen flex items-center justify-center text-center">
-            HomePage
+        <div className="h-screen flex items-center flex-col gap-5 justify-center text-center">
+            <h1 className="font-bold text-2xl">Home</h1>
+            <ul>
+                {blogs.map((blog, index) => (
+                    <li key={index}>
+                        <Link href={`/blog/${index + 1}`} className="mb-8 hover:underline">{blog}</Link>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
