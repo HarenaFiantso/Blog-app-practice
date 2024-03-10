ac1bf9bd3cc8978d71dd775ddca42d6553690ecc
HarenaFiantso <fiantsorav@gmail.com>
2024-03-10 14:48:18 +0300

    style: add some style to image

--- app/blog/[blogId]/image/page.tsx
+++ app/blog/[blogId]/image/page.tsx
@@ -1,21 +1,12 @@
 import Image from "next/image";
-import {blogs} from "@/app/datas/BlogList";
-import Link from "next/link";
 
 const BlogImagePage = ({params}: { params: { blogId: string } }) => {
-    const imageUrl = `https://picsum.photos/200`;
+    const imageUrl = `https://picsum.photos/970`;
 
     return (
-        <div>
-            <h1>Blog-{params.blogId}</h1>
-            <Image src={imageUrl} alt={`Blog ${params.blogId}`} width={120} height={120}/>
-            <ul>
-                {blogs.map((blog) => (
-                    <li key={blog.id}>
-                        <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
-                    </li>
-                ))}
-            </ul>
+        <div className="flex items-center justify-center flex-col">
+            <h1 className="mb-3 font-bold text-2xl">This is Blog {params.blogId} image</h1>
+            <Image src={imageUrl} alt={`Blog ${params.blogId}`} width={500} height={500} className="rounded-2xl"/>
         </div>
     );
 }
--- app/datas/BlogList.ts
+++ app/datas/BlogList.ts
@@ -1,7 +1,14 @@
 import {Blog} from "@/app/types/Blog";
 
 export const blogs: Blog[] = [
-    {id: 1, title: "Blog-1"},
-    {id: 2, title: "Blog-2"},
-    {id: 3, title: "Blog-3"}
+    { id: 1, title: "Blog-1" },
+    { id: 2, title: "Blog-2" },
+    { id: 3, title: "Blog-3" },
+    { id: 4, title: "Blog-4" },
+    { id: 5, title: "Blog-5" },
+    { id: 6, title: "Blog-6" },
+    { id: 7, title: "Blog-7" },
+    { id: 8, title: "Blog-8" },
+    { id: 9, title: "Blog-9" },
+    { id: 10, title: "Blog-10" }
 ];
