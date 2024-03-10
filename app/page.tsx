6ae026ba15682ec33b5e5639941349b045fee5c5
HarenaFiantso <fiantsorav@gmail.com>
2024-03-10 11:29:05 +0300

    chore: configure home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,14 @@
+"use client"
+
+import {useRouter} from "next/navigation";
+import {useEffect} from "react";
+
 export default function Home() {
-    return (
-        <div className="h-screen flex justify-center items-center">
-            <h1>Hello, this is my first blog app with NextJS</h1>
-        </div>
-    );
+    const router = useRouter();
+
+    useEffect(() => {
+        router.push('/home')
+    }, [router])
+
+    return null
 }
